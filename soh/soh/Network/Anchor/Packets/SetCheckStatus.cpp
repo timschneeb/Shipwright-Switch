b61db770207664c17fb5c54f20b8547e56fde2dd
Philip Dubé <159546+serprex@users.noreply.github.com>
2026-06-05 06:16:11 +0000

    more hardening of code in Network/ (#6650)
    also unique_ptr

--- soh/soh/Network/Anchor/Packets/SetCheckStatus.cpp
+++ soh/soh/Network/Anchor/Packets/SetCheckStatus.cpp
@@ -3,6 +3,7 @@
 #include <libultraship/libultraship.h>
 #include "soh/Enhancements/game-interactor/GameInteractor.h"
 #include "soh/OTRGlobals.h"
+#include "soh/Enhancements/randomizer/randomizerEnums/RandomizerCheck.h"
 
 static bool isResultOfHandling = false;
 
@@ -39,6 +40,10 @@ void Anchor::HandlePacket_SetCheckStatus(nlohmann::json payload) {
     auto randoContext = Rando::Context::GetInstance();
 
     RandomizerCheck rc = payload["rc"].get<RandomizerCheck>();
+    if (rc < 0 || rc >= RC_MAX) {
+        SPDLOG_ERROR("[Anchor] SET_CHECK_STATUS: rc {} out of range", (int)rc);
+        return;
+    }
     RandomizerCheckStatus status = payload["status"].get<RandomizerCheckStatus>();
     bool skipped = payload["skipped"].get<bool>();
 
